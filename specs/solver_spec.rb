3a599d189fe973be240b8d32894efd240caa9d1f
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-09-01 11:58:32 -0500

    Add reverse spec

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -2,15 +2,26 @@ require 'rspec'
 require_relative '../lib/solver'
 
 describe 'solver class' do
+  solver = Solver.new
+
   describe 'factorial' do
     it 'number factorial' do
-      expect(Solver.new.factorial(4)).to eq(24)
+      expect(solver.factorial(4)).to eq(24)
     end
     it 'return 1 if number is 0' do
-      expect(Solver.new.factorial(0)).to eq(1)
+      expect(solver.factorial(0)).to eq(1)
     end
     it 'factorial error' do
-      expect { Solver.new.factorial(-1) }.to raise_error('No negative values')
+      expect { solver.factorial(-1) }.to raise_error('No negative values')
+    end
+  end
+
+  describe 'reverse' do
+    it 'reverse string' do
+      expect(solver.reverse('hello')).to eq('olleh')
+    end
+    it 'reverse error' do
+      expect { solver.reverse(123) }.to raise_error('just strings allowed')
     end
   end
 end
