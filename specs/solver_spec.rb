414ce5bcd534ec9fe0a0e257243c6b43c2dfb842
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-09-01 11:13:03 -0500

    Factorial spec

--- specs/solver_spec.rb
+++ specs/solver_spec.rb
@@ -1,1 +1,16 @@
+require 'rspec'
 require_relative '../lib/solver'
+
+describe 'solver class' do
+  solver = Solver.new
+
+  it 'number factorial' do
+    expect(solver.factorial(4)).to eq(24)
+  end
+  it 'return 1 if number is 0' do
+    expect(Solver.new.factorial(0)).to eq(1)
+  end
+  it 'factorial exception' do
+    expect(solver.factorial(-4)).to raise_exception('no negative values')
+  end
+end
